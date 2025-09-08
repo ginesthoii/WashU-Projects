218fc0de26e02e6bc416a3fe3bdcf0c7448e3a88
Olivia Griffin <oliviagriffindev@gmail.com>
2025-09-08 16:15:09 -0500

    Add files via upload

---
+++ Modules/Module 3 - Lucky_Duck_Investigations/Roulette_Loss_Investigation/Player_Dealer_Correlation/roulette_dealer_finder_by_time_and_game.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cat $1_Dealer_schedule | awk -F " " '{print "Black Jack dealer: " $3, $4, "| Roulette dealer: " $5, $6, "| Texas Hold EM dealer: " $7, $8, "were all working during the " $1, $2, "shift."}' | grep "$2" 
+
